728a0a3858476e413b111be1fd5cb4326c9dc3fe
Roya Chobineh <72050537+royachobineh@users.noreply.github.com>
2021-09-22 18:31:01 -0400

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = ['','','','','','','',''];
+  
+    for (let i = 0; i < horizontalJoin.length; i++) {
+      for (let j = 0; j < horizontalJoin[i].length; j++) {
+        verticalJoin[j] += horizontalJoin[i][j];
+      }
     }
-}
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    for (let l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  };
 
 module.exports = wordSearch
